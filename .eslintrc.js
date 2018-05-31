382ae0324749c11035ee4c6b8bc5a9c7b75ece08
dimo89 <dmitri.karasjov@gmail.com>
2018-05-31 13:56:03 +0300

    configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,36 @@
 module.exports = {
-    "extends": "airbnb",
-    "globals": {
-      "document": true
-    }
+  "extends": "airbnb",
+  "globals": {
+    "document": true,
+    "fetch": true,
+    "console": true,
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+      "ecmaFeatures": {
+          "jsx": true,
+          "modules": true,
+          "experimentalObjectRestSpread": true
+      }
+  },
+  "plugins": [
+      "react"
+  ],
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "rules": {
+      "comma-dangle": 0,
+      "react/jsx-uses-vars": 1,
+      "react/display-name": 1,
+      "no-unused-vars": "warn",
+      "no-console": 0,
+      "no-unexpected-multiline": "warn"
+  },
+  "settings": {
+      "react": {
+          "pragma": "React",
+          "version": "15.6.1"
+      }
+  }
 };
